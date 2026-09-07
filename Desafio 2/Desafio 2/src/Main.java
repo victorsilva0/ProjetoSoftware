8fb07b04662461940cd8437fc38af73fcd84b52d
victorsilva0 <nascimento.victor985@gmail.com>
2026-09-07 17:49:56 -0300

    Add files via upload

---
+++ Desafio 2/Desafio 2/src/Main.java
@@ -0,0 +1,71 @@
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        GerenciarProdutos gerenciador = new GerenciarProdutos();
+        int opcao;
+
+        while (true) {
+            System.out.println("==== MENU DE OPÇÕES ====");
+            System.out.println("1 - Cadastrar Produto");
+            System.out.println("2 - Listar Produto");
+            System.out.println("3 - Atualizar Produto");
+            System.out.println("4 - Remover Produto");
+            System.out.println("5 - Sair");
+            System.out.print("DIGITE UMA OPÇÃO: ");
+            opcao = sc.nextInt();
+            sc.nextLine();
+
+            switch (opcao) {
+                case 1:
+                    System.out.println("Nome: ");
+                    String nome = sc.nextLine();
+                    System.out.println("Preço: ");
+                    double preco = sc.nextDouble();
+                    System.out.println("Quantidade: ");
+                    int quantidade = sc.nextInt();
+                    sc.nextLine();
+                    Produto produto = new Produto(nome, preco, quantidade);
+                    gerenciador.cadastrarProduto(produto);
+                    break;
+
+                case 2:
+                    gerenciador.listarProdutos();
+                    break;
+
+                case 3:
+                    gerenciador.listarProdutos();
+                    System.out.println("Id para atualizar: ");
+                    int idAtual = sc.nextInt();
+                    sc.nextLine();
+                    System.out.println("Novo nome: ");
+                    String novoNome = sc.nextLine();
+                    System.out.println("Novo preco: ");
+                    double novoPreco = sc.nextDouble();
+                    System.out.println("Nova quantidade: ");
+                    int novaQuantidade = sc.nextInt();
+                    gerenciador.atualizarProduto(idAtual, novoNome, novoPreco, novaQuantidade);
+                    break;
+
+                case 4:
+                    gerenciador.listarProdutos();
+                    System.out.println("Id para remover: ");
+                    int idRemove = sc.nextInt();
+                    gerenciador.removerProduto(idRemove);
+                    break;
+
+                case 5:
+                    System.out.println("Saindo...");
+                    break;
+
+                default:
+                    System.out.println("Opção invalida!");
+            }
+        }
+    }
+}
